766933872e46a80d97224ff0488ffea6418630ad
Sir Bird <sirjubjubbird@gmail.com>
2021-05-22 11:38:47 +0530

    Batch publishing changes

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-
-const dogDivs = document.querySelectorAll(".panel-doggo");
+const mainDiv = document.querySelector("#main-row");
 const searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", breedSearch);
-var searchString = "dog";
+const placeholderImage = "img/paw.png";
+var searchString = "shep";
 document.querySelector("#breed-search-box").value = searchString;
-
-console.log(searchString);
 
 getMyDoggos(searchString);
 
 function breedSearch(event) {
     event.preventDefault();
     searchString = document.querySelector("#breed-search-box").value;
-    console.log(`Searched for: ${searchString}`);
     getMyDoggos(searchString);
 }
 
-async function populateDogDiv(divIndex, dogBreed) {
+async function populateDogDiv(dogBreed) {
+    const newDiv = addNewDogDiv("New", placeholderImage);
     const imageRef = dogBreed.reference_image_id;
-    const imageRequestURL = `https://api.thedogapi.com/v1/images/${imageRef}`;
+    const imageRequestURL = `https://api.thedogapi.com/v1/images/${dogBreed.reference_image_id}`;
     const resImage = await fetch(imageRequestURL, {
         method: 'GET',
         headers: {
@@ -29,12 +27,19 @@ async function populateDogDiv(divIndex, dogBreed) {
     })
     const dataImage = await resImage.json();
     const imageURL = dataImage.url;
-    dogDivs[divIndex].style.backgroundImage = `url("${imageURL}")`;
-    const breedName = dogBreed.name;
-    dogDivs[divIndex].innerHTML = breedName;
+    try {
+        console.log(`loading background: ${dogBreed.name}`)
+        newDiv.firstElementChild.style.backgroundImage = `url("${imageURL}")`;
+        console.log(`loaded background\n`);
+    }
+    catch (err) {
+        console.log("Some error occured");
+    }
+    newDiv.firstElementChild.innerHTML = dogBreed.name;
 }
 
 async function getMyDoggos(term) {
+    console.log(`Searching for: ${term}`);
     const breedRequestURL = `https://api.thedogapi.com/v1/breeds/search?q=${term}`
     const res = await fetch(breedRequestURL, {
         method: 'GET',
@@ -50,6 +55,28 @@ async function getMyDoggos(term) {
     const cleanArray = data.filter(item => item.reference_image_id !== undefined);
     const numOfBreeds = cleanArray.length;
     console.log(`Cleaned number of breeds: ${numOfBreeds}`);
-    cleanArray.slice(0, dogDivs.length).forEach((item, idx) => populateDogDiv(idx, item));
+    document.querySelectorAll(".main-col").forEach(item => item.remove());
+    if (numOfBreeds === 0) {
+        const blankDiv = addNewDogDiv("No breeds found", placeholderImage)
+    }
+
+    else cleanArray.forEach((item) => populateDogDiv(item));
+
+}
+
+function addNewDogDiv(breedName, imageURL) {
+    const markup = `<div class="panel-doggo">${breedName}</div>`;
+    const newDiv = document.createElement("div");
+    newDiv.className = "col-md-4 col-sm-6 main-col";
+
+    const panelDiv = document.createElement("div");
+    panelDiv.className = "panel-doggo";
+    panelDiv.innerText = breedName;
+    panelDiv.style.backgroundImage = `url(${imageURL})`;
+
+    newDiv.appendChild(panelDiv);
+    mainDiv.appendChild(newDiv);
+
+    return newDiv;
 
 }
