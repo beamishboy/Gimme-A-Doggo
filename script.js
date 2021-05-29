6771727e5d4eca5b2e83eeea622b54c63ad7611e
Sir Bird <sirjubjubbird@gmail.com>
2021-05-29 15:50:25 +0530

    Batch publishing changes

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ var referenceArray = [];
 
 getAllDoggos().then(data => {
     referenceArray = data;
-    console.log(`Reference array: `);
-    console.log(referenceArray);
     getMyDoggos((searchString));
     searchForm.addEventListener("submit", breedSearch);
 });
@@ -21,29 +19,15 @@ function breedSearch(event) {
         getMyDoggos(searchString);
     }
     catch (err) {
-        console.log(`An error occured in populateDogDiv: ${err}`);
+        console.log(`An error occured in breedSearch: ${err}`);
     }
 }
 
 async function populateDogDiv(dogBreed) {
     try {
         const newDiv = addNewDogDiv("New", placeholderImage);
-        /*const imageRef = dogBreed.reference_image_id;
-        const imageRequestURL = `https://api.thedogapi.com/v1/images/${imageRef}`;
-        const resImage = await fetch(imageRequestURL, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-type': 'application/json',
-                'x-api-key': '82eed4b3-06a9-4889-81c9-31d0e354c8fa'
-            }
-        })
-        const dataImage = await resImage.json();
-        const imageURL = dataImage.url;
-        newDiv.firstElementChild.style.backgroundImage = `url(${imageURL})`;*/
         newDiv.firstElementChild.style.backgroundImage = `url(${dogBreed.url})`;
         newDiv.firstElementChild.innerHTML = dogBreed.name;
-        console.log(`${dogBreed.name} image loaded from ${dogBreed.url}`);
     }
     catch (err) {
         console.log(`An error occured in populateDogDiv: ${err}`)
@@ -52,7 +36,7 @@ async function populateDogDiv(dogBreed) {
 
 async function getAllDoggos() {
     try {
-        console.log(`*** Dowloading info of all breeds ***`);
+        console.log(`*** Downloading info of all breeds ***`);
         const breedRequestURL = `https://api.thedogapi.com/v1/breeds/`
         const res = await fetch(breedRequestURL, {
             method: 'GET',
@@ -68,7 +52,6 @@ async function getAllDoggos() {
         const cleanArray = data.filter(item => !(item.image == undefined || item.image.url == undefined || item.image.url == "" || item.image.url.trim() == ""));
         const numOfBreeds = cleanArray.length;
         console.log(`Cleaned number of breeds (with valid image data): ${numOfBreeds}`);
-        data.forEach((item, idx) => console.log(item.image.url + "\t" + idx));
         return cleanArray;
     }
     catch (err) {
@@ -79,22 +62,28 @@ async function getAllDoggos() {
 async function getMyDoggos(term) {
     try {
         console.log(`*** Searching for: ${term} ***`);
-        const breedRequestURL = `https://api.thedogapi.com/v1/breeds/search?q=${term}`
-        const res = await fetch(breedRequestURL, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-type': 'application/json',
-                'x-api-key': '82eed4b3-06a9-4889-81c9-31d0e354c8fa'
-            }
-        })
+        const cleanArray = [];
 
-        const data = await res.json();
-        console.log(`Number of search results: ${data.length}`);
-        const cleanArray = data.map(item => referenceArray.find(element => element.id === item.id)).filter(item => item != undefined).map(item => { return { name: item.name, url: item.image.url } });
+        if (term.trim() == "") {
+            cleanArray.push(...referenceArray.map(item => { return { name: item.name, url: item.image.url } }));
+        } else {
+            const breedRequestURL = `https://api.thedogapi.com/v1/breeds/search?q=${term}`
+            const res = await fetch(breedRequestURL, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-type': 'application/json',
+                    'x-api-key': '82eed4b3-06a9-4889-81c9-31d0e354c8fa'
+                }
+            })
+
+            const data = await res.json();
+            console.log(`Number of search results: ${data.length}`);
+            cleanArray.push(...data.map(item => referenceArray.find(element => element.id === item.id)).filter(item => item != undefined).map(item => { return { name: item.name, url: item.image.url } }));
+        }
+
         const numOfBreeds = cleanArray.length;
-        console.log(`Cleaned number of breeds: ${numOfBreeds} \rCleaned breeds: `);
-        console.log({ cleanArray })
+        console.log(`Clean search results: ${numOfBreeds}`);
         document.querySelectorAll(".main-col").forEach(item => item.remove());
         if (numOfBreeds === 0) {
             const blankDiv = addNewDogDiv("No breeds found", placeholderImage)
